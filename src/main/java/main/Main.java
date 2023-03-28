3e9ee993b82b9c0d085246a752729a9e7ba6a4b2
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2023-03-27 18:28:02 -0700

    Git init

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,18 @@
+/*  
+ *  Main.java
+ *  
+ *  Description: Sets up application.
+ * 
+ *  Running Project: java -jar target/Hero-Tales-1.0-SNAPSHOT.jar (DEPRECATED)
+ *
+*/
+
+package main;
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!");
+    }
+}
