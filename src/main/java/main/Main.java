dfcdb6f09f524487081e58708f765f5573ac8324
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2023-03-28 16:56:13 -0700

    Add basic gui window

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,11 +8,25 @@
 */
 
 package main;
+import javax.swing.JFrame;
 
 public class Main 
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello World!");
+        // Setup window properties
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(true);
+        window.setTitle("Java Database Interface");
+
+        // Integrate Gui into the window
+        Gui gui = Gui.getInstance();
+        window.add(gui);
+        window.pack();
+
+        // Center and display window
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
